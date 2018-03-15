d430b3e66c2baf9dbde137b405b75afec188f644
Josh Bay <joshbay@Joshs-MacBook-Pro.local>
2018-03-15 13:14:50 -0500

    updating schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,6 @@ CREATE TABLE users
     last_name VARCHAR(50) NOT NULL,
     password_hash VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
-
 )
 
 CREATE TABLE groups
@@ -22,7 +21,6 @@ CREATE TABLE groups
     user_id INT NOT NULL,
     PRIMARY KEY (id)
     FOREIGN KEY (user_id) references users(id)
-
 )
 
 CREATE TABLE user_groups
@@ -31,11 +29,27 @@ CREATE TABLE user_groups
     group_id int NOT NULL,
     FOREIGN KEY (user_id) references users(id),
 	FOREIGN KEY (group_id) references groups(id)
-
-
 )
 
+CREATE TABLE activities
+(
+    id int AUTO_INCREMEMNT NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    activity_name VARCHAR(250) NOT NULL,
+    activity_location VARCHAR(50) NOT NULL,
+    activity_price int NOT NULL,
+    notes VARCHAR(250) NOT NULL,
+    complete boolean NOT NULL DEFAULT FALSE,
+    group_id int NOT NULL,
+    user_id int NOT NULL,
+    FOREIGN KEY (user_id) references users(id),
+	FOREIGN KEY (group_id) references groups(id)
+)
 
-
-
-
+CREATE TABLE votes
+(
+    group_id int NOT NULL,
+    user_id int NOT NULL,
+    FOREIGN KEY (user_id) references users(id),
+	FOREIGN KEY (group_id) references groups(id)
+)
