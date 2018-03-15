a8af56e02d2a50aca99e6c85d1661a3fc6983249
Josh Bay <joshbay@Joshs-MacBook-Pro.local>
2018-03-15 14:35:50 -0500

    fixing price data

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,13 +14,13 @@ INSERT INTO groups (group_name, user_id)
 VALUES ("Coworkers", 1);
 
 INSERT INTO activities (category, activity_name, activity_location, activity_price, notes, group_id, user_id)
-VALUES ("Movies", "See Black Planther", "Lincoln Park", "$10", "Wakanda forever!", 1, 2);
+VALUES ("Movies", "See Black Planther", "Lincoln Park", "10", "Wakanda forever!", 1, 2);
 
 INSERT INTO activities (category, activity_name, activity_location, activity_price, notes, group_id, user_id)
-VALUES ("Sports", "Bulls Game", "United Center", "$50", "They're playing the Lakers", 1, 3);
+VALUES ("Sports", "Bulls Game", "United Center", "50", "They're playing the Lakers", 1, 3);
 
 INSERT INTO activities (category, activity_name, activity_location, activity_price, notes, group_id, user_id)
-VALUES ("TV", "West Wing marathon", "My apt", "Free", "#PresidentBartlet", 2, 1);
+VALUES ("TV", "West Wing marathon", "My apt", "0", "#PresidentBartlet", 2, 1);
 
 
 
