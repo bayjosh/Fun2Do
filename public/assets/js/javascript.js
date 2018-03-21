2e48996ea8eec8f48875ead140fd646eed129cc3
wikiwoo <wikiwoo@wikiwoos-MacBook-Air.local>
2018-03-21 10:37:16 -0500

    Styling Updates

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,3 +1,6 @@
-$('#createGroupModal').on('shown.bs.modal', function () {
-  $('#new_group_name').trigger('focus')
-})
+$( document ).ready(function() {
+  $('#createGroupModal').on('shown.bs.modal', function () {
+    $('#new_group_name').trigger('focus')
+  })
+  
+});
